5b7911ac1f62e90fdad6923fb87add2c09efa826
Antonio-Zabdiel <112569142+Antonio-Zabdiel@users.noreply.github.com>
2023-11-14 08:09:07 -0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+/*
+2 arrais con apuntadores a,b,...,e       z, y,...,v
+*/
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c, d, e;
+    int z, y, x, w, v;
+    int ptri_1, ptrf_1;
+    int ptri_z, ptrf_z;
+
+    return 0;
+}
