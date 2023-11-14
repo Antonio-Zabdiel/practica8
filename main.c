d44e1682f27d133f1dd37f35ac6a7ec0f3be0f11
Antonio-Zabdiel <112569142+Antonio-Zabdiel@users.noreply.github.com>
2023-11-14 08:09:31 -0600

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 /*
-2 arrais con apuntadores a,b,...,e       z, y,...,v
+2 arrais con apuntadores 
+a,b,...,e       el usuario mete un numero y se rellena de a a e sumando de 1 en 1
+z, y,...,v el usuario mete n y se rellena de v a z sumando de 10 en 10
+se intercambian los valores de las "tablas"
+al final se imprime de a-e y de z-v
 */
 #include <stdio.h>
 
@@ -9,6 +13,13 @@ int main()
     int z, y, x, w, v;
     int ptri_1, ptrf_1;
     int ptri_z, ptrf_z;
+ 
+    return 0;
+}
 
+
+int f1(int asd, int yu){
+    
+    
     return 0;
 }
